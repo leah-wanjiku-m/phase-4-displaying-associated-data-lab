39359d59bf6945d5f2935f03e439cf2bba05bdba
leah wanjiku <leagm22@gmail.com>
2023-03-20 10:15:42 +0300

    done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,12 @@
 class ItemsController < ApplicationController
+
+    def index
+        item= Item.all
+        render json: item, include: :user
+    end
+
+    def show
+        items=Item.find(params[:id])
+        render json: items
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+
+    def index 
+        user=User.all 
+        render json: user
+    end
+
+    def show 
+        user= User.find(params[:id])
+        render json: user, include: :items
+    end
 end
